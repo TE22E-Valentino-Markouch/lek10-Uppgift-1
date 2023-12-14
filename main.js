95c52eff17fdda6b2e0c10a6678cc66b9eb599b8
Valentino Markouch <Valentino.Markouch@elev.ga.ntig.se>
2023-12-14 14:40:00 +0100

    klar

---
+++ main.js
@@ -0,0 +1,27 @@
+const addition = () => {
+  const tal1 = parseInt(document.getElementById("tal1").value);
+  const tal2 = parseInt(document.getElementById("tal2").value);
+
+  document.getElementById("svar").value = tal1 + tal2;
+};
+
+const subtraktion = () => {
+  const tal1 = parseInt(document.getElementById("tal1").value);
+  const tal2 = parseInt(document.getElementById("tal2").value);
+
+  document.getElementById("svar").value = tal1 - tal2;
+};
+
+const multiplikation = () => {
+  const tal1 = parseInt(document.getElementById("tal1").value);
+  const tal2 = parseInt(document.getElementById("tal2").value);
+
+  document.getElementById("svar").value = tal1 * tal2;
+};
+
+const division = () => {
+  const tal1 = parseInt(document.getElementById("tal1").value);
+  const tal2 = parseInt(document.getElementById("tal2").value);
+
+  document.getElementById("svar").value = tal1 / tal2;
+};
